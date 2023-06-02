2de48d85d21df6b624f6f260db976f969217570e
Luciano <luciano.miguel.ch95@gmail.com>
2023-06-02 17:21:23 -0300

    add typescript & tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-3xl">Sanit Front End</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
