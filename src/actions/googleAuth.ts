02edd6b4edd8eb3c6ae3f06f5fc2f9979b46a32c
Luciano <luciano.miguel.ch95@gmail.com>
2023-06-16 12:06:22 -0300

    remove comment

--- src/actions/googleAuth.ts
+++ src/actions/googleAuth.ts
@@ -10,22 +10,6 @@ interface DecodedToken {
 export const handleGoogleRegister = async (credentialResponse: any) => {
   const decoded = jwt_decode(credentialResponse.credential) as DecodedToken;
   // New Google User
-  // try {
-  //   const response = await axios.post("http://localhost:3001/api/users/new", {
-  //     email: decoded.email,
-  //     password: decoded.sub,
-  //     confirmPassword: decoded.sub,
-  //   });
-  //   //customMessage("success", response.data);
-  //   console.log(response);
-
-  //   return response;
-  //   //setVariant("LOGIN");
-  // } catch (error: any) {
-  //   console.log(error);
-  //   customMessage("error", error?.response?.data);
-  // }
-
   await axios
     .post("http://localhost:3001/api/users/new", {
       email: decoded.email,
