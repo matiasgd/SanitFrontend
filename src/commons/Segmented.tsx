00bca0c7a5da78d100fee481d991c04a8e078582
Luciano <luciano.miguel.ch95@gmail.com>
2023-06-26 13:06:38 -0300

    modify segmented component

--- src/commons/Segmented.tsx
+++ src/commons/Segmented.tsx
@@ -1,7 +1,7 @@
-import { Segmented } from "antd";
+import { Radio } from "antd";
 import { Control, Controller, RegisterOptions } from "react-hook-form";
 import {
-  CalendarTwoTone,
+  CarryOutTwoTone,
   DashboardTwoTone,
   DollarTwoTone,
   FundTwoTone,
@@ -26,34 +26,29 @@ const CustomSegmented: React.FC<SegmentedProps> = ({
       rules={rules}
       render={({ field }) => {
         return (
-          <Segmented
+          <Radio.Group
             {...field}
             size="large"
             onChange={field.onChange}
-            className="block rounded-md border-0 mt-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-            options={[
-              {
-                label: "Aumentar mis Consultas",
-                value: "Aumentar mis Consultas",
-                icon: <CalendarTwoTone />,
-              },
-              {
-                label: "Aumentar mis Ingresos",
-                value: "Aumentar mis Ingresos",
-                icon: <DollarTwoTone />,
-              },
-              {
-                label: "Obtener Métricas",
-                value: "Obtener Métricas",
-                icon: <FundTwoTone />,
-              },
-              {
-                label: "Optimizar mi Tiempo",
-                value: "Optimizar mi Tiempo",
-                icon: <DashboardTwoTone />,
-              },
-            ]}
-          />
+            buttonStyle="solid"
+          >
+            <Radio.Button value="consultas">
+              <CarryOutTwoTone className="mr-2" />
+              Aumentar mis Consultas
+            </Radio.Button>
+            <Radio.Button value="ingresos">
+              <DollarTwoTone className="mr-2" />
+              Aumentar mis Ingresos
+            </Radio.Button>
+            <Radio.Button value="metricas">
+              <FundTwoTone className="mr-2" />
+              Obtener Métricas
+            </Radio.Button>
+            <Radio.Button value="time">
+              <DashboardTwoTone className="mr-2" />
+              Optimizar mi Tiempo
+            </Radio.Button>
+          </Radio.Group>
         );
       }}
     />
