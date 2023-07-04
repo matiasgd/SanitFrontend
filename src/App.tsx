5b9010a132bf555e402216685057320323aeb1a8
jalarg <jlema1990@gmail.com>
2023-07-04 11:17:52 -0300

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router";
 import AuthForm from "./components/AuthForm";
 import Dashboard from "./components/Dashboard";
-import Home from "./components/Dashboard/Home";
+import Patient from "./components/Dashboard/Home";
+import Home from "./components/Home/Home";
 
 
 
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<AuthForm />} />
       <Route path="/me" element={<Dashboard />} />
+      <Route path="/patient" element={<Patient />} />
       <Route path="/home" element={<Home />} />
       <Route path="/404">ERROR 404</Route>
     </Routes>
