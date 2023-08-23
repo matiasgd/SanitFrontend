12528983fc078077fda127eb9371cf7596204392
Luciano <luciano.miguel.ch95@gmail.com>
2023-08-23 17:50:39 -0300

    add styles

--- src/pages/Me/Dashboard.tsx
+++ src/pages/Me/Dashboard.tsx
@@ -4,7 +4,12 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { Avatar, Button } from "antd";
-import { TbStethoscope, TbUserPlus, TbMedicalCross } from "react-icons/tb";
+import {
+  TbStethoscope,
+  TbUserPlus,
+  TbReportMedical,
+  TbCoin,
+} from "react-icons/tb";
 import AppointmentsModal from "../create/AppointmentsModal";
 import PatientModal from "../create/PatientModal";
 import SearchBar from "./components/SearchBar";
@@ -18,6 +23,7 @@ const Dashboard = () => {
   // States
   const [patients, setPatients] = useState([]);
   const [appointments, setAppointments] = useState([]);
+  const [currency, setCurrency] = useState("ARS"); // ARS, USD
   const [filter, setFilter] = useState("weekly"); // weekly, monthly, yearly
   const [isOpenPatientsModal, setOpenPatientsModal] = useState(false);
   const [isOpenAppointmentsModal, setIsOpenAppointmentsModal] = useState(false);
@@ -76,34 +82,38 @@ const Dashboard = () => {
           <div className="flex flex-col gap-4 w-full px-4">
             <div className="flex flex-col w-full gap-4 p-5 shadow-lg rounded-lg">
               <div className="flex gap-4">
-                <p className="font-bold text-lg w-[20%] mt-1">
-                  Tu Actividad General
-                </p>
-                <button
-                  onClick={() => {
-                    setFilter("weekly");
-                  }}
-                >
-                  Semanal
-                </button>
-
-                <button
-                  onClick={() => {
-                    setFilter("monthly");
-                  }}
-                >
-                  Mensual
-                </button>
-
-                <button
-                  onClick={() => {
-                    setFilter("yearly");
-                  }}
-                >
-                  Anual
-                </button>
+                <div className="flex flex-col w-[30%]justify-center items-center bg-gray-200 rounded-xl p-2">
+                  <p className="font-bold text-lg">Tu Actividad General</p>
+                  <div className="flex">
+                    <Button
+                      type="link"
+                      onClick={() => {
+                        setPatients("weekly");
+                      }}
+                    >
+                      Semanal
+                    </Button>
+                    <Button
+                      type="link"
+                      onClick={() => {
+                        setFilter("monthly");
+                      }}
+                    >
+                      Mensual
+                    </Button>
+                    <Button
+                      type="link"
+                      onClick={() => {
+                        setFilter("yearly");
+                      }}
+                    >
+                      Anual
+                    </Button>
+                  </div>
+                </div>
                 <SearchBar patients={patients} appointments={appointments} />
               </div>
+
               <div className="flex flex-row w-full gap-4">
                 <div className="flex gap-4 bg-gray-200 rounded-xl p-2 w-1/3">
                   <Avatar className="bg-red-200">
@@ -137,21 +147,32 @@ const Dashboard = () => {
                     </Button>
                   </div>
                 </div>
-                <div className="flex gap-4 bg-gray-200 rounded-xl p-3 w-1/3">
+                <div className="flex gap-4 bg-gray-200 rounded-xl p-2 w-1/3">
                   <Avatar className="bg-yellow-100">
-                    <TbMedicalCross className="text-3xl text-yellow-600" />
+                    <TbReportMedical className="text-3xl text-yellow-600" />
                   </Avatar>
-                  <div className="flex-col">
-                    <p className="font-bold text-lg  mt-1">Facturación</p>
-                    <h2 className="text-[60px]">
+                  <div className="flex flex-col">
+                    <p className="font-bold text-lg  mt-1">Facturación (ARS)</p>
+                    <h2 className="text-[40px]">
                       {formatNumberWithCommas(sales)}
                     </h2>
-                    {/* <Button
+                  </div>
+                </div>
+                <div className="flex gap-4 bg-gray-200 rounded-xl p-2 w-1/3">
+                  <Avatar className="bg-green-100">
+                    <TbCoin className="text-3xl text-green-600" />
+                  </Avatar>
+                  <div className="flex-col">
+                    <p className="font-bold text-lg  mt-1">Cobros</p>
+                    <h2 className="text-[40px]">
+                      {formatNumberWithCommas(sales)}
+                    </h2>
+                    <Button
                       onClick={() => setCurrency("ARS")}
                       type="ghost"
                       className={clsx(
-                        `bg-white text-black font-bold justify-center text-center  mr-4 border-2 border-black shadow-sm shadow-black outline-none`,
-                        currency === "ARS" && "bg-yellow-200"
+                        `bg-white text-black font-bold justify-center text-center mr-2 border-2 border-black shadow-sm shadow-black outline-none`,
+                        currency === "ARS" && "bg-green-200"
                       )}
                     >
                       ARS
@@ -160,12 +181,12 @@ const Dashboard = () => {
                       onClick={() => setCurrency("USD")}
                       type="ghost"
                       className={clsx(
-                        `bg-white text-black font-bold justify-center text-center  mr-4 border-2 border-black shadow-sm shadow-black outline-none`,
-                        currency === "USD" && "bg-yellow-200"
+                        `bg-white text-black font-bold justify-center text-center mr-2 border-2 border-black shadow-sm shadow-black outline-none`,
+                        currency === "USD" && "bg-green-200"
                       )}
                     >
                       USD
-                    </Button> */}
+                    </Button>
                   </div>
                 </div>
               </div>
