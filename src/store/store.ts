1322a5944990684631583da4cbb305c52f30112f
Luciano <luciano.miguel.ch95@gmail.com>
2023-09-07 17:28:33 -0300

    modify store logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-
 import userReducer from "./user";
 
+const isDevelopment = import.meta.env.VITE_ENVIRONMENT === "development";
+
 const store = configureStore({
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    isDevelopment
+      ? getDefaultMiddleware().concat(logger)
+      : getDefaultMiddleware(),
   reducer: {
     user: userReducer,
   },
+  devTools: isDevelopment,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/store/user.ts
+++ src/store/user.ts
@@ -22,7 +22,6 @@ const initialState: UserState = user;
 const userReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(logIn, (state, action: PayloadAction<UserState>) => {
-      console.log(action.payload);
       return { ...state, ...action.payload };
     })
     .addCase(logOut, () => {
